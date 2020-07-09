0900f5efe2921b3eafa38925c5609775e4daf521
Artyom <danilinartem1997@gmail.com>
2020-07-09 17:45:32 +0300

    Base project structure

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from "../header";
+import Main from "../main";
+import Footer from "../footer";
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
+}
+
+
+export default App;
---
+++ src/components/main/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Main = () => {
+
+    return (
+        <div>
+            Main
+        </div>
+    );
+}
+
+
+export default Main;

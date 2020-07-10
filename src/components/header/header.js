b1082596959856557bdc8f6c8d2b92f1d927c171
Artyom <danilinartem1997@gmail.com>
2020-07-10 17:26:47 +0300

    Update nav links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,52 @@
 import React from "react";
 
+import logo from "../../assets/site-logo.png";
+
+import "./header.css"
+
 
 const Header = () => {
 
     return (
-        <div>
-            Header
-        </div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <div className="container border">
+                <a className="navbar-brand"
+                   href="#">
+                    <img src={logo}
+                         className="site-logo"
+                         alt="site-logo"/>
+                </a>
+
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link"
+                               href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"
+                               href="#">
+                                Pro players
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"
+                               href="#">
+                                Heroes
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link"
+                               href="#">
+                                Teams
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 

0cedec288611ec6e32bd629f88b7a219c59f58d1
Artyom <danilinartem1997@gmail.com>
2020-07-10 17:38:16 +0300

    Replace <a> to <NavLink>

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import {HashRouter} from "react-router-dom";
+
 import Header from "../header";
 import Main from "../main";
 import Footer from "../footer";
 
+
 const App = () => {
     return (
-        <>
+        <HashRouter>
             <Header/>
             <Main/>
             <Footer/>
-        </>
+        </HashRouter>
     );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import {NavLink} from "react-router-dom";
+
 import logo from "../../assets/site-logo.png";
 
 import "./header.css"
@@ -9,44 +11,49 @@ const Header = () => {
 
     return (
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <div className="container border">
-                <a className="navbar-brand"
-                   href="#">
-                    <img src={logo}
-                         className="site-logo"
+            <div className="container border force-container-padding">
+                <NavLink className="navbar-brand"
+                         to="/">
+                    <img className="site-logo"
+                         src={logo}
                          alt="site-logo"/>
-                </a>
-
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link"
-                               href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link"
-                               href="#">
-                                Pro players
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link"
-                               href="#">
-                                Heroes
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link"
-                               href="#">
-                                Teams
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                </NavLink>
+                <HeaderLinks/>
             </div>
         </nav>
+    );
+}
+
+const HeaderLinks = () => {
+    return (
+        <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link"
+                             to="/">
+                        Home
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link"
+                             to="/players">
+                        Pro players
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link"
+                             to="/heroes">
+                        Heroes
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link"
+                             to="/teams">
+                        Teams
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
     );
 }
 

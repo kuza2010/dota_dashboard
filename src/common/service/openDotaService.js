c6329fc1ef205985875b6fe8917eb25e32156e75
Artyom <danilinartem1997@gmail.com>
2020-07-24 12:17:48 +0300

    Add open dota service

---
+++ src/common/service/openDotaService.js
@@ -0,0 +1,21 @@
+class OpenDotaService {
+
+    _apiBase = "https://api.opendota.com/api"
+
+    _getResources = async (url) => {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch: ${url}, status is: ${res.status}`)
+        }
+        return await res.json();
+    }
+
+    getHeroes = async () => {
+        return this._getResources("/heroes");
+    }
+
+}
+
+
+export default OpenDotaService;

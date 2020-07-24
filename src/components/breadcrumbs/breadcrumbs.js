b9b90bc342110e43b42d858e611f03b39eb0c707
Artyom <danilinartem1997@gmail.com>
2020-07-24 12:04:28 +0300

    Add breadcrumbs component

---
+++ src/components/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import {NavLink} from "react-router-dom";
+
+const Breadcrumbs = ({links}) => {
+
+    const renderedLinks = links.map(link => {
+        const {path, title, isActive} = link;
+
+        return (
+            <li key={`link_to-${path}`}
+                className={`breadcrumb-item ${isActive ? "active" : null}`}
+            >
+                {
+                    !isActive ?
+                        <NavLink to={path}>
+                            {title}
+                        </NavLink> :
+                        title
+                }
+            </li>
+        );
+    })
+
+    return (
+        <ol className="breadcrumb">
+            {renderedLinks}
+        </ol>
+    );
+}
+
+export default Breadcrumbs;

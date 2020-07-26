204a04e41a1c7afcfc6d88be3685e35b8d0db676
Artyom <danilinartem1997@gmail.com>
2020-07-26 18:51:38 +0300

    Hero card add mouseover, mouseout events

--- src/components/hero-components/hero-card/hero-card.js
+++ src/components/hero-components/hero-card/hero-card.js
@@ -1,22 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
-import "./hero-card.css"
 import {Link} from "react-router-dom";
 
+import "./hero-card.css"
+
+
 const HeroCard = ({image, name}) => {
+
+    const [isMouseOver, setMouseOver] = useState(false);
+
     return (
         <Link to="/">
-            <div className="position-relative">
+            <div
+                onMouseOver={() => setMouseOver(state => !state)}
+                onMouseOut={() => setMouseOver(state => !state)}
+                className="position-relative"
+            >
                 <img className="hero-grid-icon"
                      src={image}
                      alt={name}
                 />
-                <div className="position-absolute her-grid-title">
+                <div className={`position-absolute hero-grid-title ${isMouseOver ? "selected" : "unselected"}`}>
                     {name}
                 </div>
             </div>
         </Link>
     );
 }
+
 
 export default HeroCard;

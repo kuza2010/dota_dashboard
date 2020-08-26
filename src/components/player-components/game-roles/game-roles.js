d688f2a6478486f40fe9d0f83ba6702a656e0dc7
Artyom <danilinartem1997@gmail.com>
2020-08-26 17:17:35 +0300

    Add spinner instead of plaint text

--- src/components/player-components/game-roles/game-roles.js
+++ src/components/player-components/game-roles/game-roles.js
@@ -7,6 +7,7 @@ import {commonStatsShape} from "../../../common/shape/shape";
 import {calculateRolesInPercent} from "../../../common/utils";
 
 import "./game-roles.css"
+import Spinner from "../../spinner";
 
 
 const GameRoles = ({carry, mid, offlane, support}) => {
@@ -14,37 +15,38 @@ const GameRoles = ({carry, mid, offlane, support}) => {
     return (
         <React.Fragment>
             <div className="row justify-content-center game-role-margin">
-                <div className="col-lg-2 text-center">
-                    <>CARRY</>
-                    <p className="lead-bold lead-shadow">{carry}</p>
-                </div>
-                <div className="col-lg-2 text-center">
-                    <>MID</>
-                    <p className="lead-bold lead-shadow">{mid}</p>
-                </div>
-                <div className="col-lg-2 text-center">
-                    <>OFFLANE</>
-                    <p className="lead-bold lead-shadow">{offlane}</p>
-                </div>
-                <div className="col-lg-2 text-center">
-                    <>SUPPORT</>
-                    <p className="lead-bold lead-shadow">{support}</p>
-                </div>
+                <RoleItem title="CARRY" content={carry}/>
+                <RoleItem title="MID" content={mid}/>
+                <RoleItem title="OFFLANE" content={offlane}/>
+                <RoleItem title="SUPPORT" content={support}/>
             </div>
-            <hr className="my-66"/>
+            <hr className="stats-divider"/>
         </React.Fragment>
     )
 };
+
+const RoleItem = ({title, content}) => {
+    return (
+        <div className="col-lg-2 text-center">
+            {title}
+            {
+                typeof content === "string"
+                    ? <p className="lead-bold lead-shadow">{content}</p>
+                    : <div>{content}</div>
+            }
+        </div>
+    )
+}
 
 
 const GameRoleContainer = ({stats, loading}) => {
     let carry, mid, offlane, support;
 
     if (loading) {
-        carry = "loading...";
-        mid = "loading...";
-        offlane = "loading...";
-        support = "loading...";
+        carry = <Spinner/>;
+        mid = <Spinner/>;
+        offlane = <Spinner/>;
+        support = <Spinner/>;
     } else {
         const percentage = calculateRolesInPercent(stats);
         carry = percentage.carry;
@@ -53,7 +55,7 @@ const GameRoleContainer = ({stats, loading}) => {
         support = percentage.support;
     }
 
-    return <GameRoles carry={carry} mid={mid} offlane={offlane} support={support} />
+    return <GameRoles carry={carry} mid={mid} offlane={offlane} support={support}/>
 };
 
 GameRoleContainer.propTypes = {

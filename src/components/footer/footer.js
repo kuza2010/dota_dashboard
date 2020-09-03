6b8a92345c2802df1abf1dcc49f60d9fbdf02d73
Artyom <danilinartem1997@gmail.com>
2020-09-03 20:53:19 +0300

    Fix footer position on small screen

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,11 +10,11 @@ const Footer = () => {
     return (
         <footer className="container">
             <div className="row">
-                <div className="col-lg-6">
+                <div className="col">
                     <h4>Dota2 dashboard</h4>
                     <p>Hand-made with <text className="text-danger">❤</text> for Dota2.</p>
                 </div>
-                <div className="col-lg-3 offset-lg-3">
+                <div className="col">
                     <blockquote className="blockquote text-right">
                         <p className="mb-0">I bow to no king.</p>
                         <span className="blockquote-footer">

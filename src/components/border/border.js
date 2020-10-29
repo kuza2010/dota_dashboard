abd2cc9e923f74c1d133f7d813b3faa8fda4c3b4
Artyom Danilin <artyom.danilin@orioninc.com>
2020-10-29 20:00:03 +0300

    Update border

--- src/components/border/border.js
+++ src/components/border/border.js
@@ -1,9 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types"
+
+import styled from 'styled-components';
 
 import "./border.css"
 
 
-const Border = () => (<div className="black_border"/>)
+const StyledBorder = styled.div`
+            border-top: ${props => props.weightPixel}px solid ${props => props.color}`
+
+const Border = ({color, weightPixel}) =>
+    <StyledBorder
+        color={color}
+        weightPixel={weightPixel}
+    />
+
+Border.propTypes = {
+    color: PropTypes.string,
+    weightPixel: PropTypes.number,
+}
+
+Border.defaultProps = {
+    color: "#080D15",
+    weightPixel: 1,
+}
 
 
 export default Border;

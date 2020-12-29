ca9b4d0b42d62ce9e2ef4bf624bc1ea69bda479a
Artyom Danilin <artyom.danilin@orioninc.com>
2020-12-29 11:47:06 +0300

    Updates

---
+++ precache-manifest.b3db7e34af37451b3722aa85967980d7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ab48d391b7c2ba504b0620fad6d55a",
+    "url": "/dota_dashboard/index.html"
+  },
+  {
+    "revision": "be8708b98722d6dd92ca",
+    "url": "/dota_dashboard/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "85be4848a30ad24dea77",
+    "url": "/dota_dashboard/static/css/main.5c7b1731.chunk.css"
+  },
+  {
+    "revision": "be8708b98722d6dd92ca",
+    "url": "/dota_dashboard/static/js/2.77672cad.chunk.js"
+  },
+  {
+    "revision": "cc7a5421045d08bfe4cc6dd0b2d4145e",
+    "url": "/dota_dashboard/static/js/2.77672cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85be4848a30ad24dea77",
+    "url": "/dota_dashboard/static/js/main.2913c9c3.chunk.js"
+  },
+  {
+    "revision": "9567b14f5e9a206bd74d",
+    "url": "/dota_dashboard/static/js/runtime-main.dbebcdca.js"
+  },
+  {
+    "revision": "a4c12ff5a6b443300c78efb914630e6a",
+    "url": "/dota_dashboard/static/media/easy-to-use.a4c12ff5.svg"
+  },
+  {
+    "revision": "194e8f7af8eb3867184fb81fe7345b95",
+    "url": "/dota_dashboard/static/media/fork.194e8f7a.svg"
+  },
+  {
+    "revision": "eb83c632b67ce4a7310feb207cf6da33",
+    "url": "/dota_dashboard/static/media/github-icon.eb83c632.svg"
+  },
+  {
+    "revision": "d32a971e474361bcb6acd31689a0306a",
+    "url": "/dota_dashboard/static/media/heroes.d32a971e.svg"
+  },
+  {
+    "revision": "a63a0620a6b3a3d3045288fcedb5b7d4",
+    "url": "/dota_dashboard/static/media/opendota.a63a0620.svg"
+  },
+  {
+    "revision": "4193926b482f3a8a104479a9df9e9899",
+    "url": "/dota_dashboard/static/media/players.4193926b.svg"
+  },
+  {
+    "revision": "07feec7658f0ce59debf0338fe9ed616",
+    "url": "/dota_dashboard/static/media/steam-logo.07feec76.svg"
+  },
+  {
+    "revision": "4a60a1eed6d0679945f00e5d23c7b8d4",
+    "url": "/dota_dashboard/static/media/steam-logo.4a60a1ee.svg"
+  },
+  {
+    "revision": "0b95663ee3ab5724e06d5cbfe559c9f9",
+    "url": "/dota_dashboard/static/media/teams.0b95663e.svg"
+  }
+]);
